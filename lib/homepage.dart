44f4ae1454c1e5e895ba5e52239ed8109a80b115
Okwor Ejike Jude <okworejikej@gmail.com>
2023-10-02 16:22:25 +0100

    second commit

---
+++ lib/homepage.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding:
+            const EdgeInsets.only(top: 20, right: 10, left: 10, bottom: 10),
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
